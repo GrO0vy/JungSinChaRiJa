4eb13a843700fc0ba91de993d2747b1b53d83676
GrO0vy <lmc2819@naver.com>
2022-09-17 22:34:11 +0900

    오류수정

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
 
 def index(request):
-    return HTTPResponse("Hello, world. You're at the polls  index.")
-# Create your views here.
+    return HttpResponse("Hello.")
